5b4b7f1043a5ff4cba7fef936a41508a949c72fa
Jihun Kim <Jihun.dev@gmail.com>
2018-07-06 00:30:22 +0900

    Feat : Chapter03/17

---
+++ 다양한_예제로_완성하는_C포인터/Chapter03/3-17/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int iarr2[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  printf("%x \n", iarr2);
+  printf("%x %x %x \n", iarr2[0], iarr2[1], iarr2[2]);
+  return 0;
+}
