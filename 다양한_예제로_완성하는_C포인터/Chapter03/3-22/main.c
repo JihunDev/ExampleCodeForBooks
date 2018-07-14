2619aa17b48b6c2ca2e678ca2680ffe530d8e26d
Ji-Hun Kim <Jihun.dev@gmail.com>
2018-07-14 18:09:43 +0900

    Add : Chapter Code

--- 다양한_예제로_완성하는_C포인터/Chapter03/3-22/main.c
+++ 다양한_예제로_완성하는_C포인터/Chapter03/3-22/main.c
@@ -3,8 +3,8 @@
 int main() {
   char carr2[2][2] = {'A', 'B', 'C', 'D'};
 
-  printf("%c %c %c %c \n", carr2[0][0], carr2[0][1], carr2[1][0], carr2[1][1]);
-  printf("%c %c %c %c \n", **carr2, *(*carr2 + 1), **(carr2 + 1), *(*(carr2 + 1) + 1));
+  printf("%x %x %x \n", carr2 + 2, *(carr2 + 2), **(carr2 + 2));
+  printf("%x %x %x \n", carr2 + 2, carr2[2], carr2[2][0]);
 
   return 0;
 }
