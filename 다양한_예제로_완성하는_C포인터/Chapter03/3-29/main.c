95b371cc6948fcb2b2b3c56d07ffc81f2ab038bc
Ji-Hun Kim <Jihun.dev@gmail.com>
2018-07-19 23:12:49 +0900

    Add : Chapter03/29

---
+++ 다양한_예제로_완성하는_C포인터/Chapter03/3-29/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int iarr2[2][3] = {10, 20, 30, 40, 50, 60};
+
+  printf("%x \n", iarr2);
+  printf("%d %d %d %d %d %d \n", iarr2[0][0], iarr2[0][1], iarr2[0][2], iarr2[1][0], iarr2[1][1], iarr2[1][2]);
+  printf("%d %d %d %d %d %d \n", iarr2[0][0], iarr2[0][1], iarr2[0][2], iarr2[0][3], iarr2[0][4], iarr2[0][5]);
+  printf("%d %d %d %d %d %d \n", iarr2[1][-3], iarr2[1][-2], iarr2[1][-1], iarr2[1][0], iarr2[1][1], iarr2[1][2]);
+
+  return 0;
+}
