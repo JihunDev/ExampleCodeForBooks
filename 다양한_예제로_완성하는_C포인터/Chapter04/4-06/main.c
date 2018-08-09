42cc146741f68b0c80c46f3f8e74090c334a748f
Ji-Hun Kim <Jihun.dev@gmail.com>
2018-08-09 22:09:20 +0900

    Add : Chapter04/06

---
+++ 다양한_예제로_완성하는_C포인터/Chapter04/4-06/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  char carr[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
+  char *cp = carr;
+
+  printf("%c %c %c %c %c %c \n", cp[0], cp[1], cp[2], cp[3], cp[4], cp[5]);
+  
+  cp = carr + 5;
+
+  printf("%c %c %c %c %c %c \n", cp[0], cp[-1], cp[-2], cp[-3], cp[-4], cp[-5]);
+
+  cp = carr + 3;
+
+  printf("%c %c %c %c %c %c \n", cp[-3], cp[-2], cp[-1], cp[0], cp[1], cp[2]);
+  
+  return 0;
+}
