704d00ef146f1ee180e613ad21095b6dfff512a4
Ji-Hun Kim <Jihun.dev@gmail.com>
2018-12-09 17:47:06 +0900

    Add : Chapter05/02

---
+++ 다양한_예제로_완성하는_C포인터/Chapter05/5-02/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	char carr[10] = "ABCD";
+
+	printf("%c%c%c%c%c\n", carr[0], carr[1], carr[2], carr[3], carr[4]);
+	printf("%d %d %d %d %d\n", carr[0], carr[1], carr[2], carr[3], carr[4]);
+
+	return 0;
+}
